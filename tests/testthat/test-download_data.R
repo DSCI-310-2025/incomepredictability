b1a9be46763fc2183b3f986897ad1ba6d87c14c2
miketham24 <michaelthamk@gmail.com>
2025-04-08 11:08:35 -0700

    fixed download_data bug and added test coverage

--- tests/testthat/test-download_data.R
+++ tests/testthat/test-download_data.R
@@ -4,8 +4,18 @@ library(here)
 source(here::here("R", "download_data.R"))
 
 
+# test_that("download_data works correctly", {
+#   test_url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data"
+#   test_output <- "housing.data"
+
+#   download_data(test_url, test_output)
+#   expect_true(file.exists(test_output))
+
+#   if (file.exists(test_output)) file.remove(test_output)
+# })
+
 test_that("download_data works correctly", {
-  test_url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data"
+  test_url <- "https://raw.githubusercontent.com/miketham24/adult_dataset/refs/heads/main/adult.csv"
   test_output <- "housing.data"
 
   download_data(test_url, test_output)
